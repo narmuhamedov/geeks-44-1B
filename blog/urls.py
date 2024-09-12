738f275a8c3889814dbf6aaa9e2179c4ddcdfb1c
rzpy1997 <kingradomir@gmail.com>
2024-09-12 18:38:09 +0600

    'four_lesson'

--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('post_list/', views.post_list_view),
-    path('post_list/<int:id>/', views.post_detail_view),
 
+    path('post_list/', views.post_list_view, name='post_list'),
+    path('post_list_delete/', views.post_list_delete_view, name='post_list_delete'),
+    path('post_list_edit/', views.post_list_edit_view, name='post_list_edit'),
+    path('post_list/<int:id>/delete/', views.post_drop_view),
+    path('post_list/<int:id>/update/', views.update_post_view),
+
+
+    path('post_list/<int:id>/', views.post_detail_view),
+    path('create_post/', views.create_post_view, name='add_news'),
     path('hello/', views.hello_view),
     path('about_me/', views.about_view),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,82 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from . import models
+from . import models, forms
+
+
+
+# UPDATE
+def post_list_edit_view(request):
+    if request.method == "GET":
+        # query запрос
+        post_object = models.Post.objects.all()
+        return render(
+            request,
+            template_name='crud/post_list_edit.html',
+            context={
+                'post_object': post_object
+            }
+        )
+
+
+def update_post_view(request, id):
+    post_id = get_object_or_404(models.Post, id=id)
+    if request.method == 'POST':
+        form = forms.PostForm(request.POST, instance=post_id)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Новость отредактирована!!! <a href = "/post_list/">На список новостей</a>')
+    else:
+        form = forms.PostForm(instance=post_id)
+
+    return render(
+        request,
+        template_name= 'crud/update_post.html',
+        context={
+            'form': form,
+            'post_id': post_id
+        }
+    )
+
+
+
+
+# DELETE NEWS
+def post_list_delete_view(request):
+    if request.method == "GET":
+        # query запрос
+        post_object = models.Post.objects.all()
+        return render(
+            request,
+            template_name='crud/post_list_delete.html',
+            context={
+                'post_object': post_object
+            }
+        )
+
+
+def post_drop_view(request, id):
+    post_id = get_object_or_404(models.Post, id=id)
+    post_id.delete()
+    return HttpResponse('Новость удалена!!! <a href = "/post_list/">На список новостей</a>')
+
+
+# CREATE NEWS
+
+def create_post_view(request):
+    if request.method == 'POST':
+        form = forms.PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Данные успешно отправлены!!! <a href = "/post_list/">На список новостей</a>')
+    else:
+        form = forms.PostForm()
+    return render(
+        request,
+        template_name='crud/create_post.html',
+        context={
+            'form': form
+        }
+    )
 
 
 # для полной информации
@@ -14,8 +90,6 @@ def post_detail_view(request, id):
                 'post_id': post_id
             }
         )
-
-
 
 
 # вывод не полной информации
