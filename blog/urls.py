20233fbe2e9d69bf8ca475f0a32d88dd72bcef6d
rzpy1997 <kingradomir@gmail.com>
2024-09-16 18:42:46 +0600

    'lesson5'

--- blog/urls.py
+++ blog/urls.py
@@ -3,15 +3,15 @@ from . import views
 
 urlpatterns = [
 
-    path('post_list/', views.post_list_view, name='post_list'),
-    path('post_list_delete/', views.post_list_delete_view, name='post_list_delete'),
-    path('post_list_edit/', views.post_list_edit_view, name='post_list_edit'),
-    path('post_list/<int:id>/delete/', views.post_drop_view),
-    path('post_list/<int:id>/update/', views.update_post_view),
+    path('post_list/', views.PostListView.as_view(), name='post_list'),
+    path('post_list_delete/', views.PostListDeleteView.as_view(), name='post_list_delete'),
+    path('post_list_edit/', views.PostUpdateListView.as_view(), name='post_list_edit'),
+    path('post_list/<int:id>/delete/', views.PostDropDeleteView.as_view()),
+    path('post_list/<int:id>/update/', views.PostEditView.as_view()),
+    path('search/', views.SearchView.as_view(), name='search'),
 
-
-    path('post_list/<int:id>/', views.post_detail_view),
-    path('create_post/', views.create_post_view, name='add_news'),
+    path('post_list/<int:id>/', views.PostDetailView.as_view()),
+    path('create_post/', views.PostCreateView.as_view(), name='add_news'),
     path('hello/', views.hello_view),
     path('about_me/', views.about_view),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,109 +1,189 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.views import generic
 from . import models, forms
+
+
+class SearchView(generic.ListView):
+    template_name = 'post_list.html'
+    context_object_name = 'post_object'
+    paginate_by = 5
+
+    def get_queryset(self):
+        return models.Post.objects.filter(title__icontains=self.request.GET.get('q')).order_by('-id')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.request.GET.get('q')
+        return context
+
+
+
 
 
 
 # UPDATE
-def post_list_edit_view(request):
-    if request.method == "GET":
-        # query запрос
-        post_object = models.Post.objects.all()
-        return render(
-            request,
-            template_name='crud/post_list_edit.html',
-            context={
-                'post_object': post_object
-            }
-        )
+class PostUpdateListView(generic.ListView):
+    template_name = 'crud/post_list_edit.html'
+    context_object_name = 'post_object'
+    model = models.Post
+
+    def get_queryset(self):
+        return self.model.objects.all().order_by('-id')
 
 
-def update_post_view(request, id):
-    post_id = get_object_or_404(models.Post, id=id)
-    if request.method == 'POST':
-        form = forms.PostForm(request.POST, instance=post_id)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('Новость отредактирована!!! <a href = "/post_list/">На список новостей</a>')
-    else:
-        form = forms.PostForm(instance=post_id)
+class PostEditView(generic.UpdateView):
+    template_name = 'crud/update_post.html'
+    form_class = forms.PostForm
+    success_url = '/post_list_edit/'
 
-    return render(
-        request,
-        template_name= 'crud/update_post.html',
-        context={
-            'form': form,
-            'post_id': post_id
-        }
-    )
+    def get_object(self, **kwargs):
+        post_id = self.kwargs.get('id')
+        return get_object_or_404(models.Post, id=post_id)
 
 
+# def post_list_edit_view(request):
+#     if request.method == "GET":
+#         # query запрос
+#         post_object = models.Post.objects.all()
+#         return render(
+#             request,
+#             template_name='crud/post_list_edit.html',
+#             context={
+#                 'post_object': post_object
+#             }
+#         )
+
+
+# def update_post_view(request, id):
+#     post_id = get_object_or_404(models.Post, id=id)
+#     if request.method == 'POST':
+#         form = forms.PostForm(request.POST, instance=post_id)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('Новость отредактирована!!! <a href = "/post_list/">На список новостей</a>')
+#     else:
+#         form = forms.PostForm(instance=post_id)
+#
+#     return render(
+#         request,
+#         template_name='crud/update_post.html',
+#         context={
+#             'form': form,
+#             'post_id': post_id
+#         }
+#     )
 
 
 # DELETE NEWS
-def post_list_delete_view(request):
-    if request.method == "GET":
-        # query запрос
-        post_object = models.Post.objects.all()
-        return render(
-            request,
-            template_name='crud/post_list_delete.html',
-            context={
-                'post_object': post_object
-            }
-        )
+class PostListDeleteView(generic.ListView):
+    template_name = 'crud/post_list_delete.html'
+    context_object_name = 'post_object'
+    model = models.Post
+
+    def get_queryset(self):
+        return self.model.objects.all().order_by('-id')
 
 
-def post_drop_view(request, id):
-    post_id = get_object_or_404(models.Post, id=id)
-    post_id.delete()
-    return HttpResponse('Новость удалена!!! <a href = "/post_list/">На список новостей</a>')
+class PostDropDeleteView(generic.DeleteView):
+    template_name = 'crud/confirm_delete.html'
+    success_url = '/post_list_delete/'
+
+    def get_object(self, **kwargs):
+        post_id = self.kwargs.get('id')
+        return get_object_or_404(models.Post, id=post_id)
+
+
+# def post_list_delete_view(request):
+#     if request.method == "GET":
+#         # query запрос
+#         post_object = models.Post.objects.all()
+#         return render(
+#             request,
+#             template_name='crud/post_list_delete.html',
+#             context={
+#                 'post_object': post_object
+#             }
+#         )
+
+
+# def post_drop_view(request, id):
+#     post_id = get_object_or_404(models.Post, id=id)
+#     post_id.delete()
+#     return HttpResponse('Новость удалена!!! <a href = "/post_list/">На список новостей</a>')
 
 
 # CREATE NEWS
+class PostCreateView(generic.CreateView):
+    template_name = 'crud/create_post.html'
+    form_class = forms.PostForm
+    success_url = '/post_list/'
 
-def create_post_view(request):
-    if request.method == 'POST':
-        form = forms.PostForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return HttpResponse('Данные успешно отправлены!!! <a href = "/post_list/">На список новостей</a>')
-    else:
-        form = forms.PostForm()
-    return render(
-        request,
-        template_name='crud/create_post.html',
-        context={
-            'form': form
-        }
-    )
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super(PostCreateView, self).form_valid(form=form)
+
+
+# def create_post_view(request):
+#     if request.method == 'POST':
+#         form = forms.PostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponse('Данные успешно отправлены!!! <a href = "/post_list/">На список новостей</a>')
+#     else:
+#         form = forms.PostForm()
+#     return render(
+#         request,
+#         template_name='crud/create_post.html',
+#         context={
+#             'form': form
+#         }
+#     )
 
 
 # для полной информации
-def post_detail_view(request, id):
-    if request.method == 'GET':
-        post_id = get_object_or_404(models.Post, id=id)
-        return render(
-            request,
-            template_name='post_detail.html',
-            context={
-                'post_id': post_id
-            }
-        )
+class PostDetailView(generic.DetailView):
+    template_name = 'post_detail.html'
+    context_object_name = 'post_id'
+
+    def get_object(self, **kwargs):
+        post_id = self.kwargs.get('id')
+        return get_object_or_404(models.Post, id=post_id)
+
+
+# def post_detail_view(request, id):
+#     if request.method == 'GET':
+#         post_id = get_object_or_404(models.Post, id=id)
+#         return render(
+#             request,
+#             template_name='post_detail.html',
+#             context={
+#                 'post_id': post_id
+#             }
+#         )
 
 
 # вывод не полной информации
-def post_list_view(request):
-    if request.method == "GET":
-        # query запрос
-        post_object = models.Post.objects.all()
-        return render(
-            request,
-            template_name='post_list.html',
-            context={
-                'post_object': post_object
-            }
-        )
+class PostListView(generic.ListView):
+    template_name = 'post_list.html'
+    context_object_name = 'post_object'
+    model = models.Post
+
+    def get_queryset(self):
+        return self.model.objects.all().order_by('-id')
+
+
+# def post_list_view(request):
+#     if request.method == "GET":
+#         # query запрос
+#         post_object = models.Post.objects.all()
+#         return render(
+#             request,
+#             template_name='post_list.html',
+#             context={
+#                 'post_object': post_object
+#             }
+#         )
 
 
 def hello_view(request):
